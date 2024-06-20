f6470aa610e2136419428a0e2980e75ef6c466da
Jakob Guldberg Aaes <jakob.aaes@res-group.com>
2024-06-20 09:07:01 +0200

    added straight as a package manager

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1,5 +1,26 @@
 ;;; Commentary:
 ;;" init.el: settings from customise-*
 
-(load "~/.emacs.d/etc/custom.el" 'noerror)
+
+;; Ensure straight.el and use-package are loaded
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;; Load Org mode as early as possible
+(straight-use-package 'org)
+
+;; load the config
 (org-babel-load-file "~/.emacs.d/config.org")
