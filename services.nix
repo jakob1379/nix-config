89b67fd6ba4b0b3913433126fc2c3c802ac38c7b
Jakob Guldberg Aaes <jakob.aaes@res-group.com>
2024-08-29 12:28:23 +0200

    updated rclone systemd service to use native programs rather than relying on pkgs.fuse

--- services.nix
+++ services.nix
@@ -1,40 +1,63 @@
 { config, pkgs, lib, ... }:
 let
-  createRcloneMountService = { name, remote, mountPath ? "/home/${config.home.username}/${name}", remotePath ? "/" }: {
+  createNativeRcloneMountService = { name, remote, mountPath ? "/home/${config.home.username}/${name}", remotePath ? "/" }: {
     Unit = {
       Description = "Rclone mount service for ${name}";
-      After = [ "network-online.target" ];
-      Requires = [ "network-online.target" ];
     };
 
     Service = {
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}";
       ExecStart = ''
-        ${pkgs.rclone}/bin/rclone mount \
+        rclone mount \
           --config /home/${config.home.username}/.config/rclone/rclone.conf \
           --vfs-fast-fingerprint \
           --vfs-cache-mode full \
+          --allow-other \
           ${remote}:${remotePath} ${mountPath}
         '';
-      ExecStop = "${pkgs.fuse3}/bin/fusermount -u ${mountPath}";
+      ExecStop = "fusermount -u ${mountPath}";
       Type = "notify";
       Restart = "on-failure";
       RestartSec = "10s";
-      Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
     };
-
     Install = {
       WantedBy = [ "default.target" ];
     };
   };
 
+  # createRcloneMountService = { name, remote, mountPath ? "/home/${config.home.username}/${name}", remotePath ? "/" }: {
+  #   Unit = {
+  #     Description = "Rclone mount service for ${name}";
+  #     After = [ "network-online.target" ];
+  #     Requires = [ "network-online.target" ];
+  #     After = [ "systemd-networkd-wait-online.service" ];  # or "systemd-networkd-wait-online.service"
+  #     Wants = [ "systemd-networkd-wait-online.service" ];   # or "systemd-networkd-wait-online.service"
+  #   };
+
+  #   Service = {
+  #     ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}";
+  #     ExecStart = ''
+  #       ${pkgs.rclone}/bin/rclone mount \
+  #         --config /home/${config.home.username}/.config/rclone/rclone.conf \
+  #         --vfs-fast-fingerprint \
+  #         --vfs-cache-mode full \
+  #         --allow-other \
+  #         ${remote}:${remotePath} ${mountPath}
+  #     #   '';
+  #     ExecStop = "${pkgs.fuse3}/fusermount -u ${mountPath}";
+  #     Type = "notify";
+  #     Restart = "on-failure";
+  #     RestartSec = "10s";
+  #     Environment = [ "PATH=/run/wrappers/bin/:$PATH" ];
+  #   };
+
+  #   Install = {
+  #     WantedBy = [ "default.target" ];
+  #   };
+  # };
+
 in
 {
-
-  home.packages = with pkgs; [
-    rclone
-    fuse3
-  ];
 
   services = {
     copyq.enable = true;
@@ -50,11 +73,11 @@ in
   systemd.user.startServices = true;
   systemd.user.services = {
     # Create the rclone mount services by calling the function with the desired parameters using named arguments
-    rclone-mount-dropbox-private = createRcloneMountService {
+    rclone-mount-dropbox-private = createNativeRcloneMountService {
       name = "dropbox-private";
       remote = "dropbox-private";
     };
-    # rclone-mount-gdrive-private = createRcloneMountService {
+    # rclone-mount-gdrive-private = createNativeRcloneMountService {
     #   name = "gdrive-private";
     #   remote = "gdrive-private";
     # };
