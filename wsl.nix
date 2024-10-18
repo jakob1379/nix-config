a85dcb193b3e65ef9ab73b62b9e28d2339a1fe6e
Jakob Guldberg Aaes <jakob1379@gmail.com>
2024-10-18 08:43:04 +0200

    updated ssh configs for wsl

--- wsl.nix
+++ wsl.nix
@@ -1,24 +1,18 @@
-{
-  config,
-  pkgs,
-  lib,
-  inputs,
-  system,
-  ...
-}:
+{ config, pkgs, lib, inputs, system, ... }:
 let
   # Import the exported lists from packages.nix
   packages = import ./packages.nix { inherit pkgs system inputs; };
-  dotfiles = import ./dotfiles.nix { inherit pkgs; };
+  dotfiles = import ./dotfiles.nix { inherit pkgs lib; };
 
   sshConfigOverride = {
     ".ssh/config".text = ''
+
+      Include ~/.ssh/local_config
+
       Host *
-        AddKeysToAgent yes
-    '';
+        AddKeysToAgent yes'';
   };
-in
-{
+in {
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -28,11 +22,12 @@ in
   home.stateVersion = "24.05";
 
   # Override to not include gui packages
-  home.packages =
-    packages.corePackages ++ packages.devPackages ++ packages.customScripts ++ packages.emacsPackages;
+  home.packages = packages.corePackages ++ packages.devPackages
+    ++ packages.customScripts ++ packages.emacsPackages;
 
   # Override the `sshConfig`
-  home.file = sshConfigOverride // (dotfiles.emacsConfig // dotfiles.mediaConfig);
+  home.file = sshConfigOverride
+    // (dotfiles.emacsConfig // dotfiles.mediaConfig);
 
   programs.home-manager.enable = true;
 
