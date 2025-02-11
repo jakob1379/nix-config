cc68f37567f08a2f47f4e8fc5f9b68d4d3a8429a
Jakob Guldberg Aaes <jakob1379@gmail.com>
2025-02-11 09:15:28 +0100

    enabled better qt5 integration for plasma6

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -73,7 +73,11 @@ in
     sddm.enable = true;
     sddm.wayland.enable = true;
   };
-  services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6 = {
+    enable = true;
+    enableQt5Integration = true;
+  };
+
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     konsole
     kate
