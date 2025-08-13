744c8dc51cf9d276680e7d0bcb4d6048b5894f06
Jakob Stender Guldberg <jakob1379gmail.com>
2025-08-13 15:09:55 +0200

    fix: Disable Netbird tray icon to fix desktop file patch issue

--- modules/base/services.nix
+++ modules/base/services.nix
@@ -138,6 +138,11 @@ in
         indicator = true;
       };
 
+      netbird = {
+        enable = true;
+        tray.enable = false;
+      };
+
       ssh-agent.enable = true;
       easyeffects.enable = true;
       mpris-proxy.enable = true;
