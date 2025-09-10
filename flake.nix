ef909c28579aab9582b774b3be14be03fc21425b
Jakob Stender Guldberg <jakob1379gmail.com>
2025-09-10 08:43:56 +0200

    refactor: moved inputs back into flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "Home Manager configuration of jga";
 
-  inputs = import ./nix/inputs.nix;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixgl.url = "github:nix-community/nixGL";
+    zen-browser.url = "github:youwen5/zen-browser-flake";
+  };
 
   outputs = inputs@{ nixpkgs, ... }: (import ./nix/outputs.nix) inputs;
 }
