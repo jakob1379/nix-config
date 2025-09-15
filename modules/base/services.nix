aa202adabd395c653aedfcfaac91e2c140802be0
Jakob Stender Guldberg <jakob1379gmail.com>
2025-09-15 15:26:39 +0200

    removed unused kdeconnect

--- modules/base/services.nix
+++ modules/base/services.nix
@@ -137,11 +137,6 @@ in
         timeout = 5;
       };
 
-      kdeconnect = {
-        enable = true;
-        indicator = true;
-      };
-
       ssh-agent.enable = true;
       easyeffects.enable = true;
       mpris-proxy.enable = true;
