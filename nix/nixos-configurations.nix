a32f6ffd2fb2e02dd00ba25f7991b2275c7e564e
Jakob Stender Guldberg <jakob1379@gmail.com>
2025-10-08 11:44:24 +0200

    nix: split nixosSystems per host (ku, yoga) instead of combining modules

--- nix/nixos-configurations.nix
+++ nix/nixos-configurations.nix
@@ -5,6 +5,13 @@
     specialArgs = { inherit inputs; };
     modules = [
       ../nixos/ku/configuration.nix
+    ];
+  };
+
+  yoga = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
+    modules = [
       ../nixos/yoga/configuration.nix
     ];
   };
