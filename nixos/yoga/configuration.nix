1745ec8ee90ec77d424ff17b9321284ccbfb7d8c
Jakob Stender Guldberg <jakob1379@gmail.com>
2025-10-08 12:16:16 +0200

    nixos(yoga): nixfmt applied and prune duplicates

--- nixos/yoga/configuration.nix
+++ nixos/yoga/configuration.nix
@@ -5,7 +5,6 @@
   ...
 }:
 let
-  LC_LOCALE = "da_DK.UTF-8";
   createRcloneMountService =
     {
       name,
@@ -18,11 +17,7 @@ let
       after = [ "network-online.target" ];
       restartIfChanged = true;
       enable = true;
-      wantedBy = [
-        "default.target"
-        "network-online.target"
-      ];
-
+      wantedBy = [ "default.target" ];
       serviceConfig = {
         ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPath}";
         ExecStart = "${pkgs.rclone}/bin/rclone mount --config /home/jga/.config/rclone/rclone.conf --vfs-fast-fingerprint --vfs-cache-mode full ${remote}:${remotePath} ${mountPath}";
@@ -33,82 +28,25 @@ let
 in
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../common.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "yoga";
 
-  # Host-specific networking
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.firewall.enable = true;
+  i18n.defaultLocale = "da_DK.UTF-8";
 
-  # Select internationalisation properties (override defaultLocale)
-  i18n.defaultLocale = "${LC_LOCALE}";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "${LC_LOCALE}";
-    LC_IDENTIFICATION = "${LC_LOCALE}";
-    LC_MEASUREMENT = "${LC_LOCALE}";
-    LC_MONETARY = "${LC_LOCALE}";
-    LC_NAME = "${LC_LOCALE}";
-    LC_NUMERIC = "${LC_LOCALE}";
-    LC_PAPER = "${LC_LOCALE}";
-    LC_TELEPHONE = "${LC_LOCALE}";
-    LC_TIME = "${LC_LOCALE}";
-  };
-
-  # Xkb layout for this host
   services.xserver.xkb = {
     layout = "dk";
     variant = "";
   };
 
-  # KDE specific overrides
   environment.plasma6.excludePackages = with pkgs.kdePackages; [ ];
 
-  # Enable sensors and audio settings (already in common, kept minimal here)
-  hardware = {
-    sensor.iio.enable = true;
-    graphics = {
-      enable = true;
-    };
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-    };
-    pulseaudio.enable = false;
-  };
-
-  # Enable docker and libvirtd (inherited from common)
-
-  # programs specific to this host
-  programs.niri.enable = true;
-
-  # DisplayLink service (host-specific)
-  systemd.services.displaylink = {
+  hardware.bluetooth = {
     enable = true;
-    description = "DisplayLink Manager";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "systemd-udevd.service" ];
-    requires = [ "systemd-udevd.service" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.displaylink}/bin/DisplayLinkManager";
-      Restart = "on-failure";
-      RestartSec = 5;
-      User = "root";
-      Group = "root";
-    };
+    powerOnBoot = true;
   };
-
-  services.xserver.videoDrivers = [
-    "displaylink"
-    "modesetting"
-    "nvidia"
-  ];
 
   hardware.nvidia = {
     modesetting.enable = true;
@@ -122,83 +60,18 @@ in
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
-  specialisation = {
-    on-the-go.configuration = {
-      system.nixos.tags = [ "on-the-go" ];
-      hardware.nvidia = {
-        prime = {
-          offload = {
-            enable = lib.mkForce true;
-            enableOffloadCmd = lib.mkForce true;
-          };
-          sync.enable = lib.mkForce false;
-        };
-      };
-    };
-  };
-
-  # User modifications: append group and packages
   users.users.jga = lib.mkForce (
     config.users.users.jga
     // {
       extraGroups = config.users.users.jga.extraGroups ++ [ "netbird-jgalabs" ];
-      packages = config.users.users.jga.packages;
     }
   );
 
-  # Enable user defined systemd services for rclone mounts
   systemd.user.services = {
     rclone-mount-dropbox-private = createRcloneMountService {
       name = "dropbox-private";
       remote = "dropbox-private";
     };
-  };
-
-  # Append displaylink to system packages
-  environment.systemPackages = lib.mkForce (
-    config.environment.systemPackages ++ [ pkgs.displaylink ]
-  );
-
-  environment.variables = {
-    KWIN_DRM_PREFER_COLOR_DEPTH = "24";
-  };
-
-  environment.etc."environment.d/desktop-environment.conf".text = ''
-    [Environment]
-    DESKTOP_SESSION=$XDG_SESSION_DESKTOP
-  '';
-
-  programs.ssh.askPassword = lib.mkForce (
-    if builtins.getEnv "DESKTOP_SESSION" == "plasma" then
-      "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass"
-    else if builtins.getEnv "DESKTOP_SESSION" == "gnome" then
-      "${pkgs.seahorse}/libexec/seahorse/ssh-askpass"
-    else
-      "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass" # Default to KDE's program
-  );
-
-  swapDevices = lib.mkForce [ ];
-
-  boot = {
-    extraModulePackages = [ config.boot.kernelPackages.evdi ];
-    initrd.kernelModules = [ "evdi" ];
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
-
-    kernelModules = [ "kvm-intel" ];
-    kernelParams = [
-      "acpi_backlight=native"
-      "psmouse.synaptics_intertouch=0"
-      "nvidia-drm.modeset=1"
-      "nvidia-drm.fbdev=1"
-    ];
-  };
-
-  # No swap
-  swapDevices = lib.mkForce [ ];
-
-  networking.firewall = {
-    enable = true;
   };
 
   system.stateVersion = config.system.stateVersion;
