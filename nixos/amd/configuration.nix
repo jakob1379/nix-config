59add4ec8b88351f315250d7840d26f1450b6576
Jakob Guldberg Aaes <jakob1379@gmali.com>
2025-12-02 21:58:04 +0100

    fix: disabled gnupg for now as it collides with keepassxc

--- nixos/amd/configuration.nix
+++ nixos/amd/configuration.nix
@@ -13,12 +13,11 @@
   programs.fuse.userAllowOther = true;
 
   i18n.defaultLocale = "da_DK.UTF-8";
-
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    pinentryPackage = pkgs.pinentry-gtk2;
-  };
+  # programs.gnupg.agent = {
+  #   enable = false;
+  #   enableSSHSupport = true;
+  #   pinentryPackage = pkgs.pinentry-gtk2;
+  # };
   services.pcscd.enable = true;
 
   services.xserver.xkb = {
