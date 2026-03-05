61b5f258259f5da6939f1a5d881ff7ef7123898e
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-03-05 16:59:11 +0100

    fix: changed to nixpkgs-untable-small for faster patches

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of jga";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
