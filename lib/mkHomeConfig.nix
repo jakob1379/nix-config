f0542954776f2633fa5642d124c06dc976c9e5a5
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-03-14 20:51:31 +0100

    feat(shikanectl): manage saved shikane profiles

--- lib/mkHomeConfig.nix
+++ lib/mkHomeConfig.nix
@@ -8,10 +8,48 @@
   lib,
 }:
 
+let
+  shikaneOverlay =
+    _: prev:
+    let
+      shikanectlWrapper = prev.writeText "shikanectl-wrapper" (
+        builtins.replaceStrings
+          [
+            "@runtimePath@"
+            "@realShikanectl@"
+          ]
+          [
+            "${
+              prev.lib.makeBinPath [
+                prev.coreutils
+                prev.difftastic
+                prev.yq-go
+              ]
+            }:$PATH"
+            "${prev.shikane}/bin/shikanectl"
+          ]
+          (builtins.readFile ../bin/shikanectl)
+      );
+    in
+    {
+      shikane = prev.symlinkJoin {
+        inherit (prev.shikane) name;
+        paths = [ prev.shikane ];
+        inherit (prev.shikane) meta;
+        passthru = prev.shikane.passthru or { };
+        postBuild = ''
+          rm "$out/bin/shikanectl"
+          cp ${shikanectlWrapper} "$out/bin/shikanectl"
+          chmod +x "$out/bin/shikanectl"
+        '';
+      };
+    };
+in
 inputs.home-manager.lib.homeManagerConfiguration {
   pkgs = import inputs.nixpkgs {
     inherit system;
     config.allowUnfreePredicate = lib.allowUnfreePredicate;
+    overlays = [ shikaneOverlay ];
   };
   modules = [
     # All systems import the common base configuration
