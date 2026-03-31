52fb42971fd16b2ce964192d2188c8d81c637344
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-03-31 14:53:59 +0200

    refactor: simplify system package overrides

--- home/systems/amd.nix
+++ home/systems/amd.nix
@@ -12,39 +12,13 @@
 
   customPackages = {
     gui.enable = lib.mkForce true;
-    core.packages = lib.mkForce (
+    exclude = with pkgs; [ btop ];
+    extra = lib.mkAfter (
       with pkgs;
       [
         (btop.override {
           rocmSupport = true;
         })
-        git-filter-repo
-        busybox
-        dconf
-        duf
-        entr
-        gdu
-        gitflow
-        gitleaks
-        glib
-        gnumake
-        unixtools.ping
-        hyperfine
-        imagemagick
-        isd
-        libqalculate
-        libsecret
-        nix-output-monitor
-        nix-prefetch-github
-        nix-search-cli
-        onefetch
-        python3Packages.keyring
-        rename
-        silver-searcher
-        speedtest-go
-        tldr
-        unar
-        yq-go
       ]
     );
     gui.packages = lib.mkAfter (
