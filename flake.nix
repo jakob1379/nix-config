b00122c15237f086325f92af70597f66a4a4c789
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-04-21 09:36:32 +0200

    add nix-index-database home-manager module

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     waytorandr = {
       url = "github:jakob1379/waytorandr";
       inputs.nixpkgs.follows = "nixpkgs";
--- home/modules/default.nix
+++ home/modules/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    inputs.nix-index-database.homeModules.default
     inputs.waytorandr.homeManagerModules.default
     ./dotfiles.nix
     ./packages.nix
