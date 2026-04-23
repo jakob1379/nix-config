f1db87fc8642b888fd566e77ad180cad23177720
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-04-23 12:07:59 +0200

    chore(amd): install colloid icon theme

--- nixos/hosts/amd/default.nix
+++ nixos/hosts/amd/default.nix
@@ -11,7 +11,7 @@
   programs.gpu-screen-recorder.enable = true;
   networking.hostName = "amd";
   programs.fuse.userAllowOther = true;
-
+  fonts.packages = [ colloid-icon-theme ];
   i18n.defaultLocale = "en_US.UTF-8";
   # programs.gnupg.agent = {
   #   enable = false;
