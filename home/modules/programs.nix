99cc2684d5e89c3cadccfd3f6e5b4dd735c5785d
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-04-28 12:11:59 +0200

    fix: make ghostty be smarter about asking if it should be closed

--- home/modules/programs.nix
+++ home/modules/programs.nix
@@ -269,7 +269,7 @@ in
             background-opacity = 0.85;
             bold-is-bright = true;
             clipboard-paste-protection = false;
-            confirm-close-surface = false;
+            confirm-close-surface = true;
             copy-on-select = "clipboard";
             cursor-style = "block";
             cursor-style-blink = false;
