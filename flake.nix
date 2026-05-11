047b7d8e333b0717536ed3428798bdecb0dd87c4
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-05-11 14:02:25 +0200

    fix(tmuux): found a nice bar

--- flake.nix
+++ flake.nix
@@ -78,35 +78,6 @@
           ${pkgs.lib.getExe package} run --all-files --config ${configFile}
         ''
       );
-      packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
-        system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfreePredicate = lib.allowUnfreePredicate;
-            overlays = [ inputs.t3code-flake.overlays.default ];
-          };
-        in
-        {
-          inherit (pkgs) t3code;
-        }
-      );
-      apps = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (
-        system:
-        let
-          packages = self.packages.${system};
-          t3codeApps = inputs.t3code-flake.apps.${system};
-        in
-        {
-          t3code = {
-            type = "app";
-            program = "${packages.t3code}/bin/t3code";
-          };
-        }
-        // nixpkgs.lib.optionalAttrs (builtins.hasAttr "t3code-desktop" t3codeApps) {
-          inherit (t3codeApps) t3code-desktop;
-        }
-      );
       devShells = forAllSystems (pkgs: {
         default =
           let
