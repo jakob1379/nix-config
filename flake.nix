491770c1d8e96692345ab9f5a8b336745b5d5c05
Jakob Guldberg Aaes <jakob1379@gmali.com>
2026-05-12 07:37:48 +0200

    fix: changed back to unstable
    small has too many things that needs to be compiled, and is not usable until I have the remote
    builder setup with cachix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of jga";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
