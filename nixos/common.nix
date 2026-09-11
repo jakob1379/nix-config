74b3477cd66c485eea5872b1c4ce05e54c5ade43
Jakob Stender Guldberg <jakob1379@gmail.com>
2026-09-11 07:51:52 +0200

    chore: updated kernel

--- nixos/common.nix
+++ nixos/common.nix
@@ -189,7 +189,7 @@
     initrd.kernelModules = [ "evdi" ];
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    kernelPackages = pkgs.linuxKernel.packages.linux_7_1;
+    kernelPackages = pkgs.linuxKernel.packages.linux_7_2;
     kernelModules = [ ];
     kernelParams = [
       "acpi_backlight=native"
